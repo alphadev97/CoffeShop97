01b2b87a8b77d98fd033c3c66c949f3e9b670069
Muhammad Usama <muhammadusama30289@gmail.com>
2024-04-16 06:41:32 +0500

    Rendering Shopping Cart Items

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,19 @@
+import CartListItem from "@/components/CartListItem";
+import { useCart } from "@/providers/CartProvider";
 import { StatusBar } from "expo-status-bar";
-import { View, Text, Platform } from "react-native";
+import { View, Text, Platform, FlatList } from "react-native";
 
 const CartScreen = () => {
+  const { items } = useCart();
+
   return (
     <View>
-      <Text>CartScreen</Text>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        contentContainerStyle={{ padding: 10, gap: 10 }}
+      />
+
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
